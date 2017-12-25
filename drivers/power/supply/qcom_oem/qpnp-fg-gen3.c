5016e092ec24c505bc448594a699a3efe7b3cc80
Sultanxda <sultanxda@gmail.com>
2017-12-25 14:17:46 -0800

    power: Don't store soc at shutdown and load it on boot
    This assumes that the battery charge won't change while the device is
    powered off, which is a very flawed assumption, so remove it.
    
    Note that this feature was already broken by the intial de-China commit
    anyway (since the pm_pon register was removed from the qpnp-power-on driver).
    
    Change-Id: I98ddda14485ea8f8308411f0d35e298a5ed0bf59
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/power/supply/qcom_oem/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom_oem/qpnp-fg-gen3.c
@@ -2894,9 +2894,8 @@ static int fg_psy_get_property(struct power_supply *psy,
 	switch (psp) {
 	case POWER_SUPPLY_PROP_CAPACITY:
 /* david.liu@bsp, 20160926 Add dash charging */
-		if(!get_extern_fg_regist_done())
-			pval->intval = get_prop_pre_shutdown_soc();
-		else if (chip->use_external_fg && external_fg
+		if (get_extern_fg_regist_done()
+				&& chip->use_external_fg && external_fg
 				&& external_fg->get_battery_soc)
 			pval->intval = external_fg->get_battery_soc();
 		else
