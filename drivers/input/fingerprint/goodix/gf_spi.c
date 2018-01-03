f255dfaa12928a04e11f977cacbe263356032e43
XiNGRZ <chenxingyu92@gmail.com>
2018-01-04 00:55:30 +0800

    fingerprint: goodix: Add proc node for disabling the sensor
    This patch is adapted from the following changes:
    
        commit 741e262e3d6e35da85f7eb273da634b7e3f384f3
        Author: fluxi <linflux@arcor.de>
        Date:   Tue Sep 13 20:55:27 2016 +0200
    
            misc: fpc1020: Honor proximity state
    
            Implement a poor man's pocket mode.
    
            * Recieve proximity state from userspace
            * Handle interrupts in an ordered workqueue
            * Additionally serialize interrupt handling
            * Disable IRQ while proximity state is near
            * Reset gpio to avoid userspace confusion (things explode if we don't)
    
        commit e12ea6648261d1af46b9b238d0edcf0fa7b7197f
        Author: Sultanxda <sultanxda@gmail.com>
        Date:   Thu Oct 20 16:11:31 2016 -0700
    
            misc: fpc1020: Simplify proximity shut-off
    
            Disabling the interrupt handler is all that is needed. Also kill the irq
            sysfs node in this case to be safe.
    
            Signed-off-by: Sultanxda <sultanxda@gmail.com>
    
    Change-Id: Icec3258f569fea3bdd82fb3b86a06b81e5909656

--- drivers/input/fingerprint/goodix/gf_spi.c
+++ drivers/input/fingerprint/goodix/gf_spi.c
@@ -41,6 +41,7 @@
 #include <linux/pm_qos.h>
 #include <linux/cpufreq.h>
 #include <linux/wakelock.h>
+#include <linux/proc_fs.h>
 /*#ifdef AMARTINZ_KILLZ_IT
 #include <linux/oneplus/boot_mode.h>
 #endif*/
@@ -635,6 +636,7 @@ static int goodix_fb_state_chg_callback(struct notifier_block *nb,
 					kill_fasync(&gf_dev->async, SIGIO, POLL_IN);
 				}
 #endif
+				gf_enable_irq(gf_dev);
 			}
 			break;
 		default:
@@ -649,6 +651,50 @@ static struct notifier_block goodix_noti_block = {
 	.notifier_call = goodix_fb_state_chg_callback,
 };
 
+static ssize_t set_disable(struct file *file,
+		const char __user *buf, size_t count, loff_t *lo)
+{
+	struct gf_dev *gf_dev = &gf;
+	char page[10] = {0};
+	int rc, val;
+
+	rc = copy_from_user(page, buf, count);
+	if (rc) {
+		return -EINVAL;
+	}
+
+	sscanf(page, "%d", &val);
+	gf_dev->disabled = !!val;
+
+	if (gf_dev->fb_black) {
+		if (val) {
+			gf_disable_irq(gf_dev);
+		} else {
+			gf_enable_irq(gf_dev);
+		}
+	}
+
+	return count;
+}
+
+static ssize_t get_disable(struct file *file,
+		char __user *buf, size_t count, loff_t *lo)
+{
+	struct gf_dev *gf_dev = &gf;
+	char page[10];
+
+	sprintf(page, "%d\n", gf_dev->disabled ? 1 : 0);
+
+	return simple_read_from_buffer(buf, count, lo, page, strlen(page));
+}
+
+static const struct file_operations proc_disable = {
+	.write = set_disable,
+	.read = get_disable,
+	.open = simple_open,
+	.owner = THIS_MODULE,
+};
+
 static struct class *gf_class;
 #if defined(USE_SPI_BUS)
 static int gf_probe(struct spi_device *spi)
@@ -657,6 +703,7 @@ static int gf_probe(struct platform_device *pdev)
 #endif
 {
 	struct gf_dev *gf_dev = &gf;
+	struct proc_dir_entry *procdir;
 	int status = -EINVAL;
 	unsigned long minor;
 	int i;
@@ -776,6 +823,9 @@ static int gf_probe(struct platform_device *pdev)
 	gf_dev->irq_enabled = 1;
 	gf_disable_irq(gf_dev);
 	gpio_set_value(gf_dev->reset_gpio, 0);
+
+	procdir = proc_mkdir("fingerprint", NULL);
+	proc_create_data("disable", S_IWUSR, procdir, &proc_disable, NULL);
 
 	pr_info("version V%d.%d.%02d\n", VER_MAJOR, VER_MINOR, PATCH_LEVEL);
 
